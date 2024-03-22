ab1f562c5ee53f05d5ab0f7c7ec83977162c9015
Rebekah Ndanu <rebekah.ndanu@student.moringaschool.com>
2024-03-22 04:27:58 +0300

    Completed assignment

---
+++ app/migrations/versions/e399fe41bb55_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e399fe41bb55
+Revises: 
+Create Date: 2024-03-22 04:22:36.122504
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e399fe41bb55'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
